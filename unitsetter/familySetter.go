0d7909cd3346da10265af8746d84ff866cb7cf03
Nick Wells <wells.nicholas@gmail.com>
2021-07-23 22:47:38 +0100

    Add FamilySetter type to set a units.Family through a parameter

---
+++ unitsetter/familySetter.go
@@ -0,0 +1,85 @@
+package unitsetter
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+
+	"github.com/nickwells/param.mod/v5/param/psetter"
+	"github.com/nickwells/strdist.mod/strdist"
+	"github.com/nickwells/units.mod/v2/units"
+)
+
+// FamilySetter is a parameter setter used to populate units.Family values.
+type FamilySetter struct {
+	psetter.ValueReqMandatory
+
+	Value **units.Family
+}
+
+// suggestAltVal will suggest a possible alternative value for the parameter
+// value. It will find those strings in the set of possible values that are
+// closest to the given value
+func (s FamilySetter) suggestAltVal(val string) string {
+	suggestedNames := ""
+	matches :=
+		strdist.CaseBlindCosineFinder.FindNStrLike(
+			3, val, units.GetFamilyNames()...)
+
+	if len(matches) > 0 {
+		sort.Strings(matches)
+		suggestedNames = " Did you mean: " +
+			strings.Join(matches, " or ") +
+			"?"
+	}
+	return suggestedNames
+}
+
+// SetWithVal (called when a value follows the parameter) checks that the
+// value can be found in the map of Units, if it cannot it returns an
+// error. If there are checks and any check is violated it returns an
+// error. Only if the value is parsed successfully and no checks are violated
+// is the Value set.
+func (s FamilySetter) SetWithVal(_ string, paramVal string) error {
+	v, err := units.GetFamily(paramVal)
+	if err != nil {
+		return fmt.Errorf("%v.%s", err, s.suggestAltVal(paramVal))
+	}
+
+	*s.Value = v
+	return nil
+}
+
+// AllowedValues returns a string describing the allowed values
+func (s FamilySetter) AllowedValues() string {
+	names := units.GetFamilyNames()
+	names = append(names, units.GetFamilyAliases()...)
+
+	sort.Strings(names)
+	rval := strings.Join(names, ", ")
+
+	return rval
+}
+
+// ValDescribe returns a string describing the value that can follow the
+// parameter
+func (s FamilySetter) ValDescribe() string {
+	return "unit-family"
+}
+
+// CurrentValue returns the current setting of the parameter value
+func (s FamilySetter) CurrentValue() string {
+	if *s.Value == nil {
+		return ""
+	}
+	return (*s.Value).Name()
+}
+
+// CheckSetter panics if the setter has not been properly created - if the
+// Value is nil.
+func (s FamilySetter) CheckSetter(name string) {
+	intro := name + ": unitsetter.FamilySetter Check failed: "
+	if s.Value == nil {
+		panic(intro + "the Value to be set is nil")
+	}
+}
---
+++ unitsetter/familySetter_test.go
@@ -0,0 +1,90 @@
+package unitsetter_test
+
+import (
+	"testing"
+
+	"github.com/nickwells/param.mod/v5/param"
+	"github.com/nickwells/param.mod/v5/paramtest"
+	"github.com/nickwells/testhelper.mod/testhelper"
+	units "github.com/nickwells/units.mod/v2/units"
+	"github.com/nickwells/unitsetter.mod/v3/unitsetter"
+)
+
+const (
+	updFlagNameFamilysetter     = "upd-gf-familysetter"
+	keepBadFlagNameFamilysetter = "keep-bad-familysetter"
+)
+
+var commonFamilysetterGFC = testhelper.GoldenFileCfg{
+	DirNames:               []string{"testdata", "FamilySetter"},
+	Pfx:                    "gf",
+	Sfx:                    "txt",
+	UpdFlagName:            updFlagNameFamilysetter,
+	KeepBadResultsFlagName: keepBadFlagNameFamilysetter,
+}
+
+func init() {
+	commonFamilysetterGFC.AddUpdateFlag()
+	commonFamilysetterGFC.AddKeepBadResultsFlag()
+}
+
+func TestFamilySetter(t *testing.T) {
+	const dfltParamName = "set-family"
+	var f *units.Family
+
+	testCases := []paramtest.Setter{
+		{
+			ID:      testhelper.MkID("bad-setter-no-value"),
+			PSetter: unitsetter.FamilySetter{},
+			ExpPanic: testhelper.MkExpPanic(
+				dfltParamName + ": unitsetter.FamilySetter Check failed: " +
+					"the Value to be set is nil"),
+		},
+		{
+			ID: testhelper.MkID("good-setter-bad-val"),
+			PSetter: unitsetter.FamilySetter{
+				Value: &f,
+			},
+			ParamVal: "nonesuch",
+			SetWithValErr: testhelper.MkExpErr(
+				`There is no unit family called "nonesuch".`),
+		},
+		{
+			ID: testhelper.MkID("good-setter-bad-val-close-match"),
+			PSetter: unitsetter.FamilySetter{
+				Value: &f,
+			},
+			ParamVal: "dostance",
+			SetWithValErr: testhelper.MkExpErr(
+				`There is no unit family called "dostance".`,
+				"Did you mean: distance?"),
+		},
+		{
+			ID: testhelper.MkID("good-setter-good-val"),
+			PSetter: unitsetter.FamilySetter{
+				Value: &f,
+			},
+			ParamVal: units.Distance,
+		},
+		{
+			ID: testhelper.MkID("good-setter-good-val-alias"),
+			PSetter: unitsetter.FamilySetter{
+				Value: &f,
+			},
+			ParamVal:     "length",
+			ValDescriber: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc.GFC = commonFamilysetterGFC
+		if tc.ParamName == "" {
+			tc.ParamName = dfltParamName
+		}
+		tc.SetVR(param.Mandatory)
+
+		f = units.SampleFamily
+
+		tc.Test(t)
+	}
+}

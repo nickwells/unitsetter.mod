f3ee2ee619f89faa2d7e837adbf9ff3a804bbea1
Nick Wells <wells.nicholas@gmail.com>
2021-07-25 23:41:27 +0100

    Add the TagSetter type for setting units.Tag values as params

---
+++ unitsetter/tagSetter_test.go
@@ -0,0 +1,82 @@
+package unitsetter_test
+
+import (
+	"testing"
+
+	"github.com/nickwells/param.mod/v5/param"
+	"github.com/nickwells/param.mod/v5/paramtest"
+	"github.com/nickwells/testhelper.mod/testhelper"
+	"github.com/nickwells/units.mod/v2/units"
+	"github.com/nickwells/unitsetter.mod/v4/unitsetter"
+)
+
+const (
+	updFlagNameTagsetter     = "upd-gf-tagsetter"
+	keepBadFlagNameTagsetter = "keep-bad-tagsetter"
+)
+
+var commonTagsetterGFC = testhelper.GoldenFileCfg{
+	DirNames:               []string{"testdata", "TagSetter"},
+	Pfx:                    "gf",
+	Sfx:                    "txt",
+	UpdFlagName:            updFlagNameTagsetter,
+	KeepBadResultsFlagName: keepBadFlagNameTagsetter,
+}
+
+func init() {
+	commonTagsetterGFC.AddUpdateFlag()
+	commonTagsetterGFC.AddKeepBadResultsFlag()
+}
+
+func TestTagSetter(t *testing.T) {
+	const dfltParamName = "set-tag"
+	var tag units.Tag
+
+	testCases := []paramtest.Setter{
+		{
+			ID:      testhelper.MkID("bad-setter-no-value"),
+			PSetter: unitsetter.TagSetter{},
+			ExpPanic: testhelper.MkExpPanic(
+				dfltParamName + ": unitsetter.TagSetter Check failed: " +
+					"the Value to be set is nil"),
+		},
+		{
+			ID: testhelper.MkID("good-setter-bad-val"),
+			PSetter: unitsetter.TagSetter{
+				Value: &tag,
+			},
+			ParamVal: "nonesuch",
+			SetWithValErr: testhelper.MkExpErr(
+				`There is no unit tag called "nonesuch".`),
+		},
+		{
+			ID: testhelper.MkID("good-setter-bad-val-close-match"),
+			PSetter: unitsetter.TagSetter{
+				Value: &tag,
+			},
+			ParamVal: "histeric",
+			SetWithValErr: testhelper.MkExpErr(
+				`There is no unit tag called "histeric".`,
+				"Did you mean: historic?"),
+		},
+		{
+			ID: testhelper.MkID("good-setter-good-val"),
+			PSetter: unitsetter.TagSetter{
+				Value: &tag,
+			},
+			ParamVal: string(units.TagHist),
+		},
+	}
+
+	for _, tc := range testCases {
+		tc.GFC = commonTagsetterGFC
+		if tc.ParamName == "" {
+			tc.ParamName = dfltParamName
+		}
+		tc.SetVR(param.Mandatory)
+
+		tag = units.Tag("")
+
+		tc.Test(t)
+	}
+}

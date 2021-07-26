9fbdbd9fd3a1dceb37b9048e607c2e83851ba5b9
Nick Wells <wells.nicholas@gmail.com>
2021-07-26 23:46:39 +0100

    Add the TagListAppender type for lists of Tags

---
+++ unitsetter/tagListAppender.go
@@ -0,0 +1,98 @@
+package unitsetter
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+
+	"github.com/nickwells/param.mod/v5/param/psetter"
+	"github.com/nickwells/strdist.mod/strdist"
+	"github.com/nickwells/units.mod/v2/units"
+)
+
+// TagListAppender is a parameter setter used to add to a slice of units.Tag
+// values.
+type TagListAppender struct {
+	psetter.ValueReqMandatory
+
+	Value *[]units.Tag
+}
+
+// suggestAltVal will suggest a possible alternative value for the parameter
+// value. It will find those strings in the set of possible values that are
+// closest to the given value
+func (s TagListAppender) suggestAltVal(val string) string {
+	suggestedNames := ""
+	matches :=
+		strdist.CaseBlindCosineFinder.FindNStrLike(
+			3, val, units.GetTagNames()...)
+
+	if len(matches) > 0 {
+		sort.Strings(matches)
+		suggestedNames = " Did you mean: " +
+			strings.Join(matches, " or ") +
+			"?"
+	}
+	return suggestedNames
+}
+
+// SetWithVal (called when a value follows the parameter) checks that the
+// value can be found in the map of Units, if it cannot it returns an
+// error. It then checks that the slice does not already contain the tag and
+// returns an error if it does.  Only if all these checks pass is the Value
+// set.
+func (s TagListAppender) SetWithVal(_ string, paramVal string) error {
+	tag := units.Tag(paramVal)
+	if !tag.IsValid() {
+		return fmt.Errorf("There is no unit tag called %q.%s",
+			tag, s.suggestAltVal(paramVal))
+	}
+
+	for _, existingTag := range *s.Value {
+		if existingTag == tag {
+			return fmt.Errorf("Tag  %q is already in the list of tags", tag)
+		}
+	}
+
+	*s.Value = append(*s.Value, tag)
+	return nil
+}
+
+// AllowedValues returns a string describing the allowed values
+func (s TagListAppender) AllowedValues() string {
+	names := units.GetTagNames()
+	sort.Strings(names)
+	rval := strings.Join(names, ", ")
+
+	return rval
+}
+
+// ValDescribe returns a string describing the value that can follow the
+// parameter
+func (s TagListAppender) ValDescribe() string {
+	return "unit-tag"
+}
+
+// CurrentValue returns the current setting of the parameter value
+func (s TagListAppender) CurrentValue() string {
+	if s.Value == nil {
+		return ""
+	}
+	rval := ""
+	sep := ""
+	for _, tag := range *s.Value {
+		rval += sep + string(tag)
+		sep = ", "
+	}
+
+	return rval
+}
+
+// CheckSetter panics if the setter has not been properly created - if the
+// Value is nil.
+func (s TagListAppender) CheckSetter(name string) {
+	intro := name + ": unitsetter.TagListAppender Check failed: "
+	if s.Value == nil {
+		panic(intro + "the Value to be set is nil")
+	}
+}
---
+++ unitsetter/tagListAppender_test.go
@@ -0,0 +1,82 @@
+package unitsetter_test
+
+import (
+	"testing"
+
+	"github.com/nickwells/param.mod/v5/param"
+	"github.com/nickwells/param.mod/v5/paramtest"
+	"github.com/nickwells/testhelper.mod/testhelper"
+	"github.com/nickwells/units.mod/v2/units"
+	"github.com/nickwells/unitsetter.mod/v4/unitsetter"
+)
+
+const (
+	updFlagNameTaglistappender     = "upd-gf-taglistappender"
+	keepBadFlagNameTaglistappender = "keep-bad-taglistappender"
+)
+
+var commonTaglistappenderGFC = testhelper.GoldenFileCfg{
+	DirNames:               []string{"testdata", "TagListAppender"},
+	Pfx:                    "gf",
+	Sfx:                    "txt",
+	UpdFlagName:            updFlagNameTaglistappender,
+	KeepBadResultsFlagName: keepBadFlagNameTaglistappender,
+}
+
+func init() {
+	commonTaglistappenderGFC.AddUpdateFlag()
+	commonTaglistappenderGFC.AddKeepBadResultsFlag()
+}
+
+func TestTagListAppender(t *testing.T) {
+	const dfltParamName = "set-tag"
+	var tags []units.Tag
+
+	testCases := []paramtest.Setter{
+		{
+			ID:      testhelper.MkID("bad-setter-no-value"),
+			PSetter: unitsetter.TagListAppender{},
+			ExpPanic: testhelper.MkExpPanic(
+				dfltParamName + ": unitsetter.TagListAppender Check failed: " +
+					"the Value to be set is nil"),
+		},
+		{
+			ID: testhelper.MkID("good-setter-bad-val"),
+			PSetter: unitsetter.TagListAppender{
+				Value: &tags,
+			},
+			ParamVal: "nonesuch",
+			SetWithValErr: testhelper.MkExpErr(
+				`There is no unit tag called "nonesuch".`),
+		},
+		{
+			ID: testhelper.MkID("good-setter-bad-val-close-match"),
+			PSetter: unitsetter.TagListAppender{
+				Value: &tags,
+			},
+			ParamVal: "histeric",
+			SetWithValErr: testhelper.MkExpErr(
+				`There is no unit tag called "histeric".`,
+				"Did you mean: historic?"),
+		},
+		{
+			ID: testhelper.MkID("good-setter-good-val"),
+			PSetter: unitsetter.TagListAppender{
+				Value: &tags,
+			},
+			ParamVal: string(units.TagHist),
+		},
+	}
+
+	for _, tc := range testCases {
+		tc.GFC = commonTagsetterGFC
+		if tc.ParamName == "" {
+			tc.ParamName = dfltParamName
+		}
+		tc.SetVR(param.Mandatory)
+
+		tags = []units.Tag{}
+
+		tc.Test(t)
+	}
+}

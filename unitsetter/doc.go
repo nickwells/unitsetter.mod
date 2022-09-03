c75d654b8ad3623b7c4976211a528c7b5ac6dfc2
Nick Wells <wells.nicholas@gmail.com>
2022-09-03 10:37:02 +0100

    refactor: minor changes to keep gofumpt quiet

--- unitsetter/doc.go
+++ unitsetter/doc.go
@@ -1,8 +1,6 @@
 /*
-
 Package unitsetter provides types which satisfies the param Setter interface
 and can be used to set various types from the units package through the
 command line.
-
 */
 package unitsetter

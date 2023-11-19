14b049913b844a3917a077b86a50d17e432a09d6
Nick Wells <wells.nicholas@gmail.com>
2023-11-19 22:41:22 +0000

    doc: fix a minor error in grammar

--- unitsetter/doc.go
+++ unitsetter/doc.go
@@ -1,5 +1,5 @@
 /*
-Package unitsetter provides types which satisfies the param Setter interface
+Package unitsetter provides types which satisfy the param.Setter interface
 and can be used to set various types from the units package through the
 command line.
 */

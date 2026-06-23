82323a3ede4cba104dadc13b74377127b292b673
Nick Wells <wells.nicholas@gmail.com>
2026-06-23 23:52:12 +0100

    feature: add methods to FamilySetter - show allowed vals and aliases
    The presence of AllowedValuesMap and AllowedValuesAliasMap methods will be
    detected by the standard help functions and will generate better help
    documentation around the Allowed Values.

--- unitsetter/familySetter.go
+++ unitsetter/familySetter.go
@@ -2,10 +2,9 @@ package unitsetter
 
 import (
 	"fmt"
-	"sort"
-	"strings"
 
 	"github.com/nickwells/param.mod/v7/psetter"
+	"github.com/nickwells/param.mod/v7/ptypes"
 	"github.com/nickwells/strdist.mod/v2/strdist"
 	"github.com/nickwells/units.mod/v2/units"
 )
@@ -41,13 +40,38 @@ func (s FamilySetter) SetWithVal(_ string, paramVal string) error {
 
 // AllowedValues returns a string describing the allowed values
 func (s FamilySetter) AllowedValues() string {
-	names := units.GetFamilyNames()
-	names = append(names, units.GetFamilyAliases()...)
+	return "a family name"
+}
 
-	sort.Strings(names)
-	rval := strings.Join(names, ", ")
+// AllowedValuesMap returns a map of allowed family names. This will be used
+// by the standard help package to generate a list of allowed values.
+func (s FamilySetter) AllowedValuesMap() ptypes.AllowedVals[string] {
+	families := units.GetFamilies()
 
-	return rval
+	avm := ptypes.AllowedVals[string]{}
+
+	for _, f := range families {
+		avm[f.Name()] = f.Description()
+	}
+
+	return avm
+}
+
+// AllowedValuesAliasMap returns a map of allowed family name aliasess. This
+// will be used by the standard help package to generate a list of aliases
+// for the allowed values.
+func (s FamilySetter) AllowedValuesAliasMap() ptypes.Aliases[string] {
+	families := units.GetFamilies()
+
+	avam := ptypes.Aliases[string]{}
+
+	for _, f := range families {
+		for _, fa := range f.FamilyAliases() {
+			avam[fa] = append(avam[fa], f.Name())
+		}
+	}
+
+	return avam
 }
 
 // ValDescribe returns a string describing the value that can follow the
